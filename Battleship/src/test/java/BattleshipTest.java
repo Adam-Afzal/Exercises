37640d3bf0615887da2b730f6d7eb967c7de5b9f
Adam-Afzal <adamafzal117@gmail.com>
2018-07-05 12:16:12 +0100

    progress on battleship

--- Battleship/src/test/java/BattleshipTest.java
+++ Battleship/src/test/java/BattleshipTest.java
@@ -7,48 +7,48 @@ import java.util.Scanner;
 import static org.junit.Assert.assertEquals;
 public class BattleshipTest {
 
-//    @Test
-//    public void makeBoardTest(){
-//
-//
-//
-//      Board board = new Board();
-//              assertEquals(3,board.getBoardSize());
-//
-//    }
-//    @Test
-//    public void addShipToBoardTest(){
-//
-//      //  PatrolBoat pat = new PatrolBoat(0,1,"Horizontal", 3);
-//        Player player = new Player(1);
-//        Destroyer des = new Destroyer(0,0,0,"Horizontal");
-//        Board board = new Board(3,3);
-//        BoardHandler bh = new BoardHandler(board,player);
-//        bh.add(des);
-//
-//
-//
-//
-//
-//    }
-//
-//    @Test
-//    public void fireTest(){
-//        Player player = new Player(1);
-//        Board board = new Board(3,3);
-//        Destroyer des = new Destroyer(0,0,0,"Horizontal");
-//
-//        BoardHandler bh = new BoardHandler(board,player);
-//        bh.add(des);
-//
-//        assertEquals("s",bh.fire(0,0));
-//
-//
-//
-//
-//    }
-//
-//    @Test
+    @Test
+    public void makeBoardTest(){
+
+
+
+      Board board = new Board();
+              assertEquals(3,board.getBoardSize());
+
+    }
+    @Test
+    public void addShipToBoardTest(){
+
+      //  PatrolBoat pat = new PatrolBoat(0,1,"Horizontal", 3);
+        Player player = new Player(1);
+        Destroyer des = new Destroyer(0,0,0,"Horizontal");
+        Board board = new Board(3,3);
+        BoardHandler bh = new BoardHandler(board,player);
+        bh.add(des);
+
+
+
+
+
+    }
+
+    @Test
+    public void fireTest(){
+        Player player = new Player(1);
+        Board board = new Board();
+        Destroyer des = new Destroyer(0,0,0,"Horizontal");
+        Board board = new Board(3,3);
+        BoardHandler bh = new BoardHandler(board,player);
+        bh.add(des);
+
+        assertEquals("s",bh.fire(0,0));
+
+
+
+
+    }
+
+    @Test
 
 
 
